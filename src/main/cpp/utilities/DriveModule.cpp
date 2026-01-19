9944eac19b86970711afa0c108fbdc641374eb2a
milana <milana@robocats.org>
2026-01-19 19:56:37 +1100

    Set up direction encoder, created skeleton functions for speed/direction motors

---
+++ src/main/cpp/utilities/DriveModule.cpp
@@ -0,0 +1,11 @@
+#include <utilities/DriveModule.h>
+
+DriveModule::DriveModule(
+    int speedMotorID, int directionMotorID, int directionEncoderID,
+    turn_t magOffset
+) : m_speedMotor
+
+{
+
+    
+}
