3d578aa61d31ed7d22bb48c8561cf3ffcc44d3f9
dana <dana@robocats.org>
2026-01-30 20:58:56 +1100

    created skeleton shooter subsystem

---
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -0,0 +1,11 @@
+#include "subsystems/ShooterSubsystem.h"
+
+ShooterSubsystem::ShooterSubsystem(){
+
+
+
+
+}
+
+
+
---
+++ src/main/include/subsystems/ShooterSubsystem.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+
+
+class ShooterSubsystem : public frc2::SubsystemBase {
+
+public:
+    ShooterSubsystem();
+
+
+
+
+
+private:
+
+
+};
