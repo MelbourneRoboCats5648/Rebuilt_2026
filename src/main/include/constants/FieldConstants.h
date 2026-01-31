6d95bbae11b7c088b15c31d8c54bdf38b09c00a3
milana <milana@robocats.org>
2026-01-31 16:09:58 +1100

    created fieldConstants file

---
+++ src/main/include/constants/FieldConstants.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "units/length.h"
+#include "units/angle.h"
+#include "units/acceleration.h"
+
+using namespace units::length;
+using namespace units::angle;
+using namespace units::acceleration;
+
+namespace FieldConstants {
+
+    meter_t HubHeight(1.888);
+    meters_per_second_squared_t gravity(9.8);
+
+    
+    
+}
