62e0d97631ec6bbacde71d5ac694957729242fc0
Shabana <Shabana@robocats.com>
2026-02-14 19:19:05 +1100

    Added Intake subsystem skeleton code

---
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -0,0 +1,4 @@
+#include <subsystems/IntakeSubsystem.h>
+
+    IntakeSubsystem::IntakeSubsystem() {}
+
---
+++ src/main/include/subsystems/IntakeSubsystem.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/SubsystemBase.h>
+
+class IntakeSubsystem : public frc2::SubsystemBase {
+public:
+    /* constructor */
+    IntakeSubsystem();
+
+private:
+
+};
