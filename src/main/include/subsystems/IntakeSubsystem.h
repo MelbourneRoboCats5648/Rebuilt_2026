c9b6c95eb22283ad7675e467e99b30106701ee7c
Shabana <Shabana@robocats.com>
2026-02-14 19:56:35 +1100

    Added Intake constants. Created function to calculate the intake wheel speed

--- src/main/include/subsystems/IntakeSubsystem.h
+++ src/main/include/subsystems/IntakeSubsystem.h
@@ -3,11 +3,13 @@
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
 
+#include <constants/IntakeConstants.h>
+
 class IntakeSubsystem : public frc2::SubsystemBase {
 public:
     /* constructor */
     IntakeSubsystem();
 
 private:
-
+    turns_per_second_t CalculateIntakeSpeed(meters_per_second_t forwardRobotSpeed);
 };
