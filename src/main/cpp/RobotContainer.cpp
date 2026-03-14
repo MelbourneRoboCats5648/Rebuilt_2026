22ea65f538d8da84a0e3613f5389ba1c41244e69
Vinh Nguyen <ngtv0404@gmail.com>
2026-03-14 13:51:04 +1100

    bind controls

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -83,7 +83,8 @@ void RobotContainer::ConfigureBindings() {
     m_mechController.POVDown().OnTrue(m_shooter.DecreaseFlywheelVelocity());
     m_mechController.POVLeft().OnTrue(m_shooter.ResetFlywheelVelocity());
 
-    m_mechController.RightTrigger().WhileTrue(m_climb.ClimbUpCommand());
+    m_mechController.LeftTrigger().WhileTrue(m_climb.ExtendCommand());
+    m_mechController.RightTrigger().WhileTrue(m_climb.RetractCommand());
 
    
     // m_shooter.SetDefaultCommand(frc2::RunCommand(
