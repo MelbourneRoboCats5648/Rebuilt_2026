71120c2725d84f15be7993320d28b47e92bfdead
dana <dana@robocats.org>
2026-04-25 16:17:38 +1000

    providing references to the flywheel, hood and feeder subsystem

--- src/main/cpp/subsystems/ShooterSubsystem.cpp
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -1,5 +1,10 @@
 #include <subsystems/ShooterSubsystem.h>
 
-ShooterSubsystem::ShooterSubsystem() {
+ShooterSubsystem::ShooterSubsystem(FlyWheelSubsystem& flyWheel, HoodSubsystem& hood, FeederSubsystem& feeder)
+: m_flyWheel(flyWheel),
+  m_hood(hood),
+  m_feeder(feeder) 
+{
+
 
 }
--- src/main/include/subsystems/ShooterSubsystem.h
+++ src/main/include/subsystems/ShooterSubsystem.h
@@ -2,7 +2,17 @@
 
 #include <frc2/command/SubsystemBase.h>
 
-class ShooterSubsytem : public frc2::SubsystemBase {
+#include <subsystems/FlyWheelSubsystem.h>
+#include <subsystems/HoodSubsystem.h>
+#include <subsystems/FeederSubsystem.h>
 
+class ShooterSubsystem : public frc2::SubsystemBase {
+    public:
+    ShooterSubsystem(FlyWheelSubsystem& flyWheel, HoodSubsystem& hood, FeederSubsystem& feeder);
+
+    private:
+    FlyWheelSubsystem& m_flyWheel;
+    HoodSubsystem& m_hood;
+    FeederSubsystem& m_feeder;
 
 };
